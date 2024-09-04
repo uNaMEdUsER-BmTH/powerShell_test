90e8366e68702a786da60161b3532e3e9e656418
uNaMEdUsER666 <masondwithers@gmail.com>
2024-09-04 14:49:30 +0000

    fixit lol

--- test.ps1
+++ test.ps1
@@ -3,13 +3,7 @@ $Win = $H.UI.RawUI.WindowSize
 $Win.Height = 30
 $Win.Width  = 80
 $H.UI.RawUI.Set_WindowSize($Win)
-$Read = Read-Host "What is the string of characters you use to refer to the mass of particles you call yourself?"
-echo "Greetings $Read, for what purpose are you using this program?"
-echo "A-What program?"
-echo "B-For the same purpose it was created for."
-echo "C-..."
-echo "D-I know your home address"
-$Jerry = Read-Host "Response?"
+$Read = Read-Host "First off, what is the string of characters you use to refer to the mass of particles you call yourself?"
 Function Ask-Questions() {
 	Param(
 	$Question,
@@ -19,7 +13,7 @@ Function Ask-Questions() {
 	$CorrectOption
 	)	
 	echo $Question
-	$Answer = Read-Host "idk"
+	$Answer = Read-Host "Answer"
 	echo $Option1
    	echo $Option2
     	echo $Option3
@@ -28,4 +22,5 @@ Function Ask-Questions() {
     	echo "Correct"}
 	else {echo "wrong"}
 }
-Ask-Questions -Question "?" -Option1  "1" -Option2  "2" -Option3  "3" -CorrectOption "1"
+Ask-Questions -Question "Test?" -Option1  "1:Yes" -Option2  "2:No" -Option3  "3:Uhh" -CorrectOption "1"
+Ask-Questions -Question "Greetings $Read, for what purpose are you using this program?" -Option1 "A-What program?" -Option2 "B-For the same purpose it was created for." -Option3 "I know your home address" -CorrectOption "C"
